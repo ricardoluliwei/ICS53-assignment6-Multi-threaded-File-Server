825e25870ca3c38fb3908af62013da038959db20
Liwei Lu <liweil2@circinus-33.ics.uci.edu>
2021-03-06 20:42:14 -0800

    header

---
+++ Server/server.c
@@ -0,0 +1,15 @@
+//Qiwei He 47771452 and Liwei Lu 90101531
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <semaphore.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
